242915fbde840ea4ee72e3daae79ae28fb79facd
Jack Bittiner <jackbittiner@hotmail.com>
2017-05-24 14:31:36 +0100

    extracted initializer and oscillator into new files

---
+++ public/scripts/initializer.js
@@ -0,0 +1,16 @@
+function init() {
+  audioContext = new AudioContext();
+  timerWorker = new Worker("/scripts/metronomeWorker.js")
+
+  timerWorker.onmessage = function(e) {
+    if (e.data == "tick") {
+      scheduler();
+    } else {
+      console.log("Message: " + e.data);
+    };
+  };
+
+  timerWorker.postMessage({"interval":lookahead})
+}
+
+window.addEventListener('load', init);
