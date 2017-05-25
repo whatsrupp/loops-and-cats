0a76f2df9c8a351d697b55ddfa2a6e2858cb6aab
Vivien Tang <vhjtang@gmail.com>
2017-05-25 17:57:39 +0100

    removed comments in initializer

--- public/scripts/models/initializer.js
+++ public/scripts/models/initializer.js
@@ -1,22 +1,3 @@
-// function init() {
-//   var audioContext = new AudioContext();
-//   var timerWorker = new Worker("/scripts/masterBeater.js")
-//
-//   timerWorker.onmessage = function(e) {
-//     if (e.data == "tick") {
-//       scheduler();
-//     } else {
-//       console.log("Message: " + e.data);
-//     };
-//   };
-//
-//
-//   timerWorker.postMessage({"interval":lookahead})
-//
-//   play()
-// }
-//
-// window.addEventListener('load', init);
 loopFactory = new LoopFactory();
 waveMaker = new WaveMaker();
 var audioSrc = '/audio/Silence.ogg';
@@ -28,6 +9,3 @@ populateDivsWithLoops = function() {
 };
 
 populateDivsWithLoops();
-
-// for(var i=1; i <= 8; i++){
-//   if(document.getElementById('#waveform' + i.toString()) === ""){
