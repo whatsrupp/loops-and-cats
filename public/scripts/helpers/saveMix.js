6433d7fb33b55522807444708043579473f1edf1
Nick Rupp <nickrupp95@gmail.com>
2017-05-30 16:56:43 +0000

    Get mix saver working but very messily

--- public/scripts/helpers/saveMix.js
+++ public/scripts/helpers/saveMix.js
@@ -8,10 +8,6 @@
   var audioDownload;
   var recorder;
   var trackNodes = []
-  var channels = [
-    [0, 0],
-    [1, 0]
-  ];
   var chunks = []
 
   getTrackNodes = function(){
@@ -22,29 +18,53 @@
   }
 
   stopSaveMix = function(){
-    recorder.stop();
+    mixRecorder.stop();
   }
 
-  startSaveMix = function(audioContext){
-    audio = audioContext;
-    merger = audio.createChannelMerger(8);
-    // splitter = audio.createChannelSplitter(2);
-    mixedAudio = audio.createMediaStreamDestination();
-    getTrackNodes();
+  // startSaveMix = function(audioContext){
+  //   audio = audioContext;
+  //   // merger = audio.createChannelMerger(8);
+  //   // mixedAudio = audio.createMediaStreamDestination();
+  //   getTrackNodes();
+  //
+  //   for(var i = 0; i < trackNodes.length; i++){
+  //     trackNodes[i].connect(merger, 0, i);
+  //   }
+  //   merger.connect(mixedAudio);
+  //   merger.connect(audio.destination)
+  //   recorder = new MediaRecorder(mixedAudio.stream);
+  //   recorder.start();
+  //
+  //   recorder.ondataavailable = function(event) {
+  //     chunks.push(event.data);
+  //   };
+  //
+  //   recorder.onstop = function(event) {
+  //     var blob = new Blob(chunks, {
+  //       "type": "audio/ogg; codecs=opus"
+  //     });
+  //     audioDownload = URL.createObjectURL(blob);
+  //     loopFactory.loops[0].updateURL(audioDownload)
+  //     var a = document.createElement("a");
+  //     var description = 'Wag2'
+  //     a.download = description + "." + blob.type.replace(/.+\/|;.+/g, "");
+  //     a.href = audioDownload;
+  //     a.innerHTML = a.download;
+  //     player.src = audioDownload;
+  //     document.body.appendChild(a);
+  //     document.body.appendChild(player);
+  //   };
+  //
+  // }
 
-    for(var i = 0; i < trackNodes.length; i++){
-      trackNodes[i].connect(merger, 0, i);
-    }
-    merger.connect(mixedAudio);
-    merger.connect(audio.destination)
-    recorder = new MediaRecorder(mixedAudio.stream);
-    recorder.start();
+  startSaveMix = function(){
+    mixRecorder.start()
 
-    recorder.ondataavailable = function(event) {
+    mixRecorder.ondataavailable = function(event) {
       chunks.push(event.data);
     };
 
-    recorder.onstop = function(event) {
+    mixRecorder.onstop = function(event) {
       var blob = new Blob(chunks, {
         "type": "audio/ogg; codecs=opus"
       });
@@ -59,7 +79,6 @@
       document.body.appendChild(a);
       document.body.appendChild(player);
     };
-
   }
 
   // initializeSaveMixButton = function(){
